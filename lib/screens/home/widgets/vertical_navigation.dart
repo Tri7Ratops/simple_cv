9e7b4b17adc91accc5f6695886761daffe19ed57
Martin JNR <pro.martin.januario@gmail.com>
2021-05-01 19:49:19 +0200

    [+] Add more personalization on vertical_navigation widget

--- lib/screens/home/widgets/vertical_navigation.dart
+++ lib/screens/home/widgets/vertical_navigation.dart
@@ -8,10 +8,13 @@ import 'package:visibility_detector/visibility_detector.dart';
 // ignore: must_be_immutable
 class VerticalNavigationItem extends Equatable {
   final Widget page;
-  final Icon icon;
+  final IconData icon;
   final String? iconTitle;
   final Function()? onMostVisible;
   final Function()? lostFocus;
+  final Color focusBackgroundColor;
+  final Color focusIconColor;
+  final Color focusTextColor;
 
   double visibilityPercentage = 0.0;
   bool mostVisible = false;
@@ -22,6 +25,9 @@ class VerticalNavigationItem extends Equatable {
     this.iconTitle,
     this.onMostVisible,
     this.lostFocus,
+    this.focusBackgroundColor = Colors.white,
+    this.focusIconColor = Colors.black,
+    this.focusTextColor = Colors.black,
   });
 
   @override
@@ -33,24 +39,26 @@ class VerticalNavigationItem extends Equatable {
         mostVisible,
         onMostVisible,
         lostFocus,
+        focusBackgroundColor,
       ];
 }
 
 class VerticalNavigation extends StatefulWidget {
   final List<VerticalNavigationItem> pages;
   final double navigationWidth;
-  final Color? focusedColor;
-  final Color? unFocusedColor;
-  final Color? navigationBackgroundColor;
+  final Color navigationBackgroundColor;
+  final Color defaultIconColor;
+  final BoxDecoration decorationItem;
 
-  const VerticalNavigation({
+  VerticalNavigation({
     Key? key,
     required this.pages,
     this.navigationWidth = 75,
-    this.focusedColor,
-    this.unFocusedColor,
-    this.navigationBackgroundColor,
-  }) : super(key: key);
+    this.navigationBackgroundColor = Colors.white10,
+    this.defaultIconColor = Colors.black,
+    BoxDecoration? decoration,
+  })  : decorationItem = decoration ?? BoxDecoration(),
+        super(key: key);
 
   @override
   _VerticalNavigationState createState() => _VerticalNavigationState();
@@ -147,12 +155,15 @@ class _VerticalNavigationState extends State<VerticalNavigation> {
               },
               child: Container(
                 padding: const EdgeInsets.all(10),
-                color: widget.pages[index].mostVisible ? Colors.white : widget.navigationBackgroundColor ?? Colors.purple,
+                decoration: widget.decorationItem.copyWith(color: widget.pages[index].mostVisible ? item.focusBackgroundColor : widget.navigationBackgroundColor),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    widget.pages[index].icon,
+                    Icon(
+                      widget.pages[index].icon,
+                      color: item.focusIconColor,
+                    ),
                     SizedBox(
                       height: 16,
                     ),
@@ -163,6 +174,7 @@ class _VerticalNavigationState extends State<VerticalNavigation> {
                           child: Text(
                             item.iconTitle ?? "",
                             overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: item.focusTextColor),
                           ),
                         ),
                       ),
@@ -182,8 +194,11 @@ class _VerticalNavigationState extends State<VerticalNavigation> {
             },
             child: Container(
               padding: const EdgeInsets.all(10),
-              color: widget.pages[index].mostVisible ? Colors.white : widget.navigationBackgroundColor ?? Colors.purple,
-              child: widget.pages[index].icon,
+              decoration: widget.decorationItem.copyWith(color: widget.pages[index].mostVisible ? Colors.white : widget.navigationBackgroundColor),
+              child: Icon(
+                widget.pages[index].icon,
+                color: widget.defaultIconColor,
+              ),
             ),
           ),
         );
@@ -194,7 +209,7 @@ class _VerticalNavigationState extends State<VerticalNavigation> {
       height: MediaQuery.of(context).size.height,
       padding: const EdgeInsets.all(10),
       width: widget.navigationWidth,
-      color: widget.navigationBackgroundColor ?? Colors.purple,
+      color: widget.navigationBackgroundColor,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: list,
