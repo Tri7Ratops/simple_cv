9890f441e5a33931cdb5f170e5a69e80d9a0e3f6
Martin JNR <pro.martin.januario@gmail.com>
2021-05-04 22:35:41 +0200

    [+] Content AOUT ME page

--- lib/screens/home/widgets/content_about_me.dart
+++ lib/screens/home/widgets/content_about_me.dart
@@ -1,63 +1,65 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ContentAboutMe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-        Text("PAGE 1"),
-      ],
+    return Container(
+      child: Column(
+        children: [
+          _header(context),
+          SizedBox(
+            height: 16,
+          ),
+          _content(context),
+          SizedBox(
+            height: 50,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      child: Center(
+        child: Text(
+          "About me".toUpperCase(),
+          style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  _content(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/icon_user.png",
+            width: MediaQuery.of(context).size.width / 2,
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Hi!",
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
+      ),
     );
   }
 }
